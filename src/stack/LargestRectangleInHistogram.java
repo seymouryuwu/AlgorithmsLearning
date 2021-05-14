e46e31c4629ab0b4f9f0ca8979fb85a8ec6821f6
YuWu-Seymour <78057679+seymouryuwu@users.noreply.github.com>
2021-05-15 01:36:59 +1000

    Revert "Revert "LargestRectangleHistogram - a neat solution""
    This reverts commit 08eb889b93a295490f6c9ee941879b7daf36a7ae.

--- src/stack/LargestRectangleInHistogram.java
+++ src/stack/LargestRectangleInHistogram.java
@@ -41,6 +41,36 @@ public class LargestRectangleInHistogram {
 
         stack.push(rectangle);
     }
+
+    // same approach
+    // but stack stores the indexes of the heights of every rectangles.
+    // and instead count the width of left side and right side separately,
+    // my width will be (the index of the one who kicks me out) - (the index of the one who is just below me in the stack) - 1.
+    public int largestRectangleArea2(int[] heights) {
+        if (heights == null || heights.length == 0) {
+            return 0;
+        }
+
+        // this is a monotonic stack stores the indexes of the heights of every rectangle
+        // pop out rectangles when they are higher than the one which is poshed in.
+        Stack<Integer> stack = new Stack<>();
+        int largest = 0;
+
+        for (int i = 0; i <= heights.length; i++) {
+            int height = (i == heights.length) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && heights[stack.peek()] >= height) {
+                // pop the top
+                int h = heights[stack.pop()];
+                int w = (stack.isEmpty()) ? i : (i - stack.peek() - 1);
+                largest = Math.max(largest, h * w);
+            }
+
+            stack.push(i);
+        }
+
+        return largest;
+    }
 }
 
 class Rectangle {
