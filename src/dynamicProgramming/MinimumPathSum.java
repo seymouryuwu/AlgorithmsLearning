b57c5216385fd40577c67b249adedc3d9841e551
YuWu-Seymour <78057679+seymouryuwu@users.noreply.github.com>
2021-05-23 21:12:47 +1000

    optimize space complexity

--- src/dynamicProgramming/MinimumPathSum.java
+++ src/dynamicProgramming/MinimumPathSum.java
@@ -24,4 +24,29 @@ public class MinimumPathSum {
 
         return minSum[minSum.length - 1][minSum[0].length - 1];
     }
+
+    // Optimize space complexity with scrolling array
+    public int minPathSum2(int[][] grid) {
+        int[][] minSum = new int[2][grid[0].length];
+
+        minSum[0][0] = grid[0][0];
+        for (int i = 1; i < grid[0].length; i++) {
+            minSum[0][i] = minSum[0][i - 1] + grid[0][i];
+        }
+
+        int old = 0;
+        int now = 1;
+        for (int i = 1; i < grid.length; i++) {
+            minSum[now][0] = minSum[old][0] + grid[i][0];
+            for (int j = 1; j < grid[0].length; j++) {
+                minSum[now][j] = Math.min(minSum[old][j], minSum[now][j - 1]) + grid[i][j];
+            }
+
+            int temp = old;
+            old = now;
+            now = temp;
+        }
+
+        return minSum[old][grid[0].length - 1];
+    }
 }
